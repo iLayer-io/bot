422e10c8e0a7889807924a80fdb6c8865db881ef
BePP <gabrio.tognozzi@gmail.com>
2025-01-25 15:31:06 +0100

    fix jest esm

--- src/viem/viem.service.ts
+++ src/viem/viem.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { http, createClient, PublicClient, publicActions } from 'viem';
+import {
+  http,
+  createClient,
+  publicActions,
+  Client,
+  Chain,
+  Account,
+  PublicRpcSchema,
+  PublicActions,
+  HttpTransport,
+} from 'viem';
 import { foundry } from 'viem/chains';
 import { CustomConfigService } from '../config/config.service.js';
 import { privateKeyToAccount } from 'viem/accounts';
 
 @Injectable()
 export class ViemService {
-  private client: PublicClient;
+  private client: Client<
+    HttpTransport,
+    Chain,
+    Account,
+    PublicRpcSchema,
+    PublicActions<HttpTransport, Chain>
+  >;
 
   constructor(private configService: CustomConfigService) {
     const rpcUrl = this.configService.botConfig.rpcUrl;
@@ -19,7 +35,7 @@ export class ViemService {
       account: privateKeyToAccount(privateKey),
     });
 
-    // @ts-expect-error: expected error
+    // @ts-expect-error error
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
     this.client = client.extend(publicActions);
   }
